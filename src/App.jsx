15f3b9dfa58a904ad1f6dcc02380408f3db2df84
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-20 23:01:20 +0600

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
+import {BrowserRouter, Routes} from "react-router-dom";
 
 const App = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <button className="btn">Button</button>
-            <button className="btn btn-neutral">Neutral</button>
-            <button className="btn btn-primary">Primary</button>
-            <button className="btn btn-secondary">Secondary</button>
-            <button className="btn btn-accent">Accent</button>
-            <button className="btn btn-ghost">Ghost</button>
-            <button className="btn btn-link">Link</button>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" component={<HomePage/>} />
+                </Routes>
+            </BrowserRouter>
 
         </div>
     );
